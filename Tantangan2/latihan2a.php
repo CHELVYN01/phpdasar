34dade126af27147ad723c637b74b1b609d58c17
CHELVYN01 <kledenkelvin9@gmail.com>
2022-10-08 20:02:54 +0700

    menambahkan margin dan padding pada style di table line 11 dan 12

--- Tantangan2/latihan2a.php
+++ Tantangan2/latihan2a.php
@@ -7,12 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body {
-            margin: auto;
-        }
-
         table {
+            margin: auto;
+            padding: auto;
             border: black solid 3px;
+
 
         }
     </style>
